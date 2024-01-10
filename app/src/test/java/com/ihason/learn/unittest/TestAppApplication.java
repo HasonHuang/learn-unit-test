cb48a055b18a0808ee0703e5f597b03a622115fc
HasonHuang <258831020@qq.com>
2024-01-10 23:06:59 +0800

    test: update mockito test case

--- app/src/test/java/com/ihason/learn/unittest/TestAppApplication.java
+++ app/src/test/java/com/ihason/learn/unittest/TestAppApplication.java
@@ -5,9 +5,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestAppApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TestAppApplication.class, args);
-    }
-
 }
--- app/src/test/java/com/ihason/learn/unittest/app/service/CreatePersonAppServiceMockitoTests.java
+++ app/src/test/java/com/ihason/learn/unittest/app/service/CreatePersonAppServiceMockitoTests.java
@@ -1,8 +1,12 @@
 package com.ihason.learn.unittest.app.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.ihason.learn.unittest.app.dto.request.CreatePersonRequest;
 import com.ihason.learn.unittest.app.dto.response.PersonDTO;
+import com.ihason.learn.unittest.domain.person.Person;
 import com.ihason.learn.unittest.domain.person.PersonService;
+import org.json.JSONException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +14,11 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.skyscreamer.jsonassert.JSONAssert;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -29,15 +36,22 @@ public class CreatePersonAppServiceMockitoTests {
     private PersonService domainService;
 
     @Test
-    @DisplayName("保存对象")
-    public void should_create_person() {
+    @DisplayName("给定合法名字-当创建用户-那么成功创建")
+    public void should_created_when_create_given_nameAndDefaultAge() throws JsonProcessingException, JSONException {
         // given
         CreatePersonRequest request = new CreatePersonRequest();
         request.setName("Tom");
+
+        Person person = Person.from(request.getName(), 100);
+        given(domainService.create(eq(request.getName()), anyInt())).willReturn(person);
+
         // when
         PersonDTO dto = service.create(request);
+
         // then
         verify(domainService).create(eq(request.getName()), ArgumentMatchers.intThat(i -> i > 0));
+        String json = JsonMapper.builder().build().writeValueAsString(dto);
+        JSONAssert.assertEquals("{id: null, age: 100, name:'Tom'}", json, true);
     }
 
 }
